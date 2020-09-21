fb9dcc3374cd95354ec6a5d082fa0c028f9b5b0d
Twist-Chen <18323430332@163.com>
2020-09-21 16:36:21 +0800

    postcss-loader autoprefixer配合使用 解决css中浏览器厂商前缀问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,40 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/index.js'
+    main: "./src/index.js",
   },
   module: {
-    rules: [{
-      test: /\.(png|png|gif)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          // name为引入文件的原名字，ext为原后缀, hash为随机hash值，更多可在webpack官网上查询
-          name: '[name]_[hash].[ext]',
-          outputPath: 'images/',
-          limit: 20480 // 1024字节 = 1KB,大于则使用file-loader
-        }
-      }
-    }, {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }, {
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }]
+    rules: [
+      {
+        test: /\.(png|png|gif)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            // name为引入文件的原名字，ext为原后缀, hash为随机hash值，更多可在webpack官网上查询
+            name: "[name]_[hash].[ext]",
+            outputPath: "images/",
+            limit: 20480, // 1024字节 = 1KB,大于则使用file-loader
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
+      },
+    ],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  }
-}
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
